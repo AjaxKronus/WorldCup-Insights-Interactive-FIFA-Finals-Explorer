9775b4e7eaade95cc31faaf12d9b263654a6b43b
AjaxKronus <107383559+AjaxKronus@users.noreply.github.com>
2026-03-25 17:08:24 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import gameData from './data/fifa-matches-2018.json';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App gameData={gameData} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
